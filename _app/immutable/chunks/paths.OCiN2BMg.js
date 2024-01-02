315bdcadefc3597c1a39877fd28ec2f0834a15e8
wduf <wduf02@gmail.com>
2024-01-02 12:29:44 -0500

    more multi-page fixes to live

--- _app/immutable/chunks/paths.OCiN2BMg.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_9r9irh?.base??"",a=globalThis.__sveltekit_9r9irh?.assets??s;export{a,s as b};
---
+++ _app/immutable/chunks/paths.dZde-W6u.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_elgjcv?.base??"",e=globalThis.__sveltekit_elgjcv?.assets??s;export{e as a,s as b};
