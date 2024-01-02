2e40689c42eee3594ffc09f57f080be4ad0c169c
wduf <wduf02@gmail.com>
2024-01-02 11:56:00 -0500

    image fix hopefully

--- _app/immutable/chunks/paths.I66Csomm.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1f2ohy6?.base??"",a=globalThis.__sveltekit_1f2ohy6?.assets??s;export{a,s as b};
---
+++ _app/immutable/chunks/paths.OCiN2BMg.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_9r9irh?.base??"",a=globalThis.__sveltekit_9r9irh?.assets??s;export{a,s as b};
