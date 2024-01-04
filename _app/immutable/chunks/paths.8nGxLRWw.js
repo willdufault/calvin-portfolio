5d46422a00e819e10c5093a89f27f98dbf35b52b
wduf <wduf02@gmail.com>
2024-01-04 14:28:39 -0500

    all images are now jpgs, should speed up load time

---
+++ _app/immutable/chunks/paths.8nGxLRWw.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_rcq4j4?.base??"",a=globalThis.__sveltekit_rcq4j4?.assets??s;export{a,s as b};
--- _app/immutable/chunks/paths.tL2NCTrE.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_yqmmd0?.base??"",a=globalThis.__sveltekit_yqmmd0?.assets??s;export{a,s as b};
