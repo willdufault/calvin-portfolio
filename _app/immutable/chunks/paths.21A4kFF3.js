bc6db03666b1fdc85bce62072ea08712cc9b1926
wduf <wduf02@gmail.com>
2024-01-03 19:55:44 -0500

    added as values to preloads

---
+++ _app/immutable/chunks/paths.21A4kFF3.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_11bq5px?.base??"",a=globalThis.__sveltekit_11bq5px?.assets??s;export{a,s as b};
--- _app/immutable/chunks/paths.__JKfGPG.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1wd9yi9?.base??"",a=globalThis.__sveltekit_1wd9yi9?.assets??s;export{a,s as b};
