019452c89f6f35654416e537263a410839da9897
wduf <wduf02@gmail.com>
2024-01-03 19:48:04 -0500

    added hero image preloading links to html

--- _app/immutable/chunks/paths.14KiW0Sf.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1h7elzo?.base??"",e=globalThis.__sveltekit_1h7elzo?.assets??s;export{e as a,s as b};
---
+++ _app/immutable/chunks/paths.__JKfGPG.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1wd9yi9?.base??"",a=globalThis.__sveltekit_1wd9yi9?.assets??s;export{a,s as b};
