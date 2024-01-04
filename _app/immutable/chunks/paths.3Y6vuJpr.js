ccf28a99b9b61e11e657f087c68c8bbfaccf1370
wduf <wduf02@gmail.com>
2024-01-03 19:23:10 -0500

    shrunk images in carousel

---
+++ _app/immutable/chunks/paths.3Y6vuJpr.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1fn4zva?.base??"",a=globalThis.__sveltekit_1fn4zva?.assets??s;export{a,s as b};
--- _app/immutable/chunks/paths.HE1pjaF3.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_r4bdyp?.base??"",a=globalThis.__sveltekit_r4bdyp?.assets??s;export{a,s as b};
