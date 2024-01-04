fcefe9f8ab5ea7802de03e4fec306303255dfe80
wduf <wduf02@gmail.com>
2024-01-03 19:26:07 -0500

    shrunk all images

---
+++ _app/immutable/chunks/paths.1PG8vxZm.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_k14x95?.base??"",a=globalThis.__sveltekit_k14x95?.assets??s;export{a,s as b};
--- _app/immutable/chunks/paths.3Y6vuJpr.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1fn4zva?.base??"",a=globalThis.__sveltekit_1fn4zva?.assets??s;export{a,s as b};
